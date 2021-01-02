2135cd3af39f634c1ea480a4ed0e9c4727a44157
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2021-01-02 13:50:54 +0200

    change build header

--- compress/compress.rb
+++ compress/compress.rb
@@ -3,13 +3,14 @@
 require_relative 'parser.rb'
 
 
-def build_header(code_type, file_size, word_size)
+def build_header(code_type, word_size)
   code_flag = code_type == 1 ? '0' : '1'
-  padding = "%04d" % (file_size % word_size).to_s(2)
+  # To encode our word size in 4 bits, we subtract 1, so that 1111 denote length of 16 and so on
+  word_flag = "%04d" % (word_size - 1).to_s(2)
 
-  code_flag + padding
+  code_flag + word_flag
 end
 
 parameters = parse_args
-puts build_header(parameters[:type], parameters[:file_size], parameters[:word_size])
+puts build_header(parameters[:type], parameters[:word_size])
 
