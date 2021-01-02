a7e89d498b93d10391bbb3f2a6ee76691b49cd12
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2021-01-02 13:00:28 +0200

    add build header

--- compress/compress.rb
+++ compress/compress.rb
@@ -2,4 +2,14 @@
 
 require_relative 'parser.rb'
 
-puts parse_args
+
+def build_header(code_type, file_size, word_size)
+  code_flag = code_type == 1 ? '0' : '1'
+  padding = "%04d" % (file_size % word_size).to_s(2)
+
+  code_flag + padding
+end
+
+parameters = parse_args
+puts build_header(parameters[:type], parameters[:file_size], parameters[:word_size])
+
